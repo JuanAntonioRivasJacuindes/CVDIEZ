491a63ece2a615f50f11e912f940f376b2c72160
Juan Antonio Rivas Jacuindes <juan.rivasj@tvazteca.com.mx>
2023-06-19 21:29:41 -0600

    register component

--- src/components/Auth/RegisterComponent.js
+++ src/components/Auth/RegisterComponent.js
@@ -15,34 +15,48 @@ export const RegisterComponent = () => {
 
     const authService = new AuthService();
     const [inputsDisable,setInputsdisable]=useState(false);
-    const [password, setPassword] = useState('');
     const [checked, setChecked] = useState(false);
-
+    
+    const [emailValid,setEmailValid]=useState()
     const [email, setEmail] = useState('');
+    
+    const [name, setName] = useState('');
+    const [nameValid, setNameValid] = useState();
+    
+    const [password, setPassword] = useState('');
+    const [passwordValid, setPasswordValid] = useState();
+    const [confirmPassword, setConfirmPassword] = useState('');
+    
     const [loginButton, setLoginButton] = useState(false);
 
-    const login = () => {
-        if (validator.isEmail(email)) {
-            setLoginButton(true)
-            setInputsdisable(true)
-            authService.getLogin(email, password)
-                .then(function (response) {
-                    if (response.data.token) {
-                        console.log(response.data.token)
-                        localStorage.setItem('AuthToken', response.data.token)
-                        window.location.reload();
-                    } else {
-
-                        console.log("error al iniciar sesion");
-                    }
-
-                    setLoginButton(false)
-                    setInputsdisable(false)
-                });
+    const register = () => {
+        setLoginButton(true)
+        setInputsdisable(true)
+        if (!validator.isEmail(email)) {
+            setEmailValid(false)
         }else{
-         
+            setEmailValid(true)
+
+        }
+        if (!validator.equals(password,confirmPassword)|validator.isEmpty(password)) {
+            setPasswordValid(false)
+        }else{
+            setPasswordValid(true)
+
+        }
+        if(validator.isEmpty(name)){
+            setNameValid(false)
+        }else{
+            setNameValid(true)
+
         }
 
+
+        if(emailValid&&passwordValid&&nameValid){
+            console.log("procedemos al login")
+        }
+        setLoginButton(false)
+        setInputsdisable(false)
     }
     return (
         <div >
@@ -60,25 +74,25 @@ export const RegisterComponent = () => {
                             <label htmlFor="Name" className="block text-900 text-xl font-medium mb-2">
                                 Nombre
                             </label>
-                            <InputText disabled={inputsDisable} inputid="Name" value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Email" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
+                            <InputText disabled={inputsDisable} inputid="Name" value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Nombre" className={nameValid?"w-full md:w-30rem mb-5":'p-invalid w-full md:w-30rem mb-5' }style={{ padding: '1rem' }} />
                             <label htmlFor="email1" className="block text-900 text-xl font-medium mb-2">
                                 Email
                             </label>
-                            <InputText disabled={inputsDisable} inputid="email1" value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Email" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
+                            <InputText disabled={inputsDisable} inputid="email1" value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Email" className={emailValid?"w-full md:w-30rem mb-5":'p-invalid w-full md:w-30rem mb-5' } style={{ padding: '1rem' }} />
 
                             <label htmlFor="password1" className="block text-900 font-medium text-xl mb-2">
                                 Contraseña
                             </label>
-                            <Password disabled={inputsDisable}  inputid="password2" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" toggleMask feedback={false} className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"></Password>
+                            <Password disabled={inputsDisable}  inputid="password2" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña"  className={passwordValid?"w-full md:w-30rem mb-5":'p-invalid w-full md:w-30rem mb-5' } toggleMask feedback={false} inputClassName="w-full p-3 md:w-30rem"></Password>
                             <label htmlFor="password2" className="block text-900 font-medium text-xl mb-2">
                                 Repetir Contraseña
                             </label>
-                            <Password disabled={inputsDisable}  inputid="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" toggleMask feedback={false} className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"></Password>
+                            <Password disabled={inputsDisable}  inputid="password1" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Contraseña"className={passwordValid?"w-full md:w-30rem mb-5":'p-invalid w-full md:w-30rem mb-5' }  toggleMask feedback={false}  inputClassName="w-full p-3 md:w-30rem"></Password>
 
                             <div className="flex align-items-center justify-content-between mb-5 gap-5">
                                 
                             </div>
-                            <Button loading={loginButton} label='Iniciar Sesión' className="w-full p-3 text-xl my-1 " onClick={() => login()}></Button>
+                            <Button loading={loginButton} label='Iniciar Sesión' className="w-full p-3 text-xl my-1 " onClick={() => register()}></Button>
                         </div>
                         <div>
 
